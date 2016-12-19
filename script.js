f7ab35aa8c4a32d0bbfc8faa325b6d8b030df30a
sajeetharan <sajeetharan@duosoftware.com>
2016-12-19 15:24:00 +0530

    Changed github account to sajeetharan

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-
 var app = angular.module("app", []);
 
 app.service("githubService", function($http, $q) {
 
-  var deferred = $q.defer();
+    var deferred = $q.defer();
 
-  this.getAccount = function() {
-    return $http.get('https://api.github.com/users/haroldrv')
-      .then(function(response) {
-        // promise is fulfilled
-        deferred.resolve(response.data);
-        return deferred.promise;
-      }, function(response) {
-        // the following line rejects the promise 
-        deferred.reject(response);
-        return deferred.promise;
-      });
-  };
+    this.getAccount = function() {
+        return $http.get('https://api.github.com/users/sajeetharan')
+            .then(function(response) {
+                // promise is fulfilled
+                deferred.resolve(response.data);
+                return deferred.promise;
+            }, function(response) {
+                // the following line rejects the promise 
+                deferred.reject(response);
+                return deferred.promise;
+            });
+    };
 });
 
 app.controller("promiseController", function($scope, $q, githubService) {
 
-  githubService.getAccount()
-    .then(
-      function(result) {
-        // promise was fullfilled (regardless of outcome)
-        // checks for information will be peformed here
-        $scope.account = result;
-      },
-      function(error) {
-        // handle errors here
-        console.log(error.statusText);
-      }
-    );
+    githubService.getAccount()
+        .then(
+            function(result) {
+                // promise was fullfilled (regardless of outcome)
+                // checks for information will be peformed here
+                $scope.account = result;
+            },
+            function(error) {
+                // handle errors here
+                console.log(error.statusText);
+            }
+        );
 });
